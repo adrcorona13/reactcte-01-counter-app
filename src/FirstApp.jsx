2dcb0afa363f9df86c3383b293d4eef96ce034b9
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-14 19:42:22 -0600

    utilizar fragment abreviado

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -2,9 +2,9 @@ import { Fragment } from "react"
 
 export const FirstApp = () => {
     return (
-        <Fragment>
+        <>
             <h1>Adrian</h1>
             <p>subtitulo</p>
-        </Fragment>
+        </>
     )
 }
