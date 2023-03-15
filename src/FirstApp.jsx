8263cabf1670fde8dbca26b97692c8c0373ae3f6
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-14 19:54:53 -0600

    impresion de variables en html

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,17 @@
 export const FirstApp = () => {
-    return (
-        <>
-            <h1>Adrian</h1>
-            <p>subtitulo</p>
-        </>
-    )
+  return (
+    <>
+      <h1>{Message(name)}</h1>
+      <code>{JSON.stringify(obj)}</code>
+      <p>subtitulo</p>
+    </>
+  );
+};
+
+const name = 'Adrian';
+const obj = {
+    a: 'a',
+    b: 2
 }
+
+const Message = (name) => `Hola ${name}`;
