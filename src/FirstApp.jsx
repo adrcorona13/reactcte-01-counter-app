ac30e523c4a6426e9f1c38023408c15a0a13e43c
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-14 19:37:56 -0600

    crear componentes

---
+++ src/FirstApp.jsx
@@ -0,0 +1,3 @@
+export const FirstApp = () => {
+    return <h1>Adrian</h1>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelloWorldApp } from "./HelloWorldApp";
+import { FirstApp } from "./FirstApp";
 
-function App(){
-    return <h1>Hola Mundo!!!!</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App></App>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelloWorldApp></HelloWorldApp>
+    <FirstApp></FirstApp>
+  </React.StrictMode>
+);
