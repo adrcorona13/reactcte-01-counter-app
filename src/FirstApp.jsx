3e346759cd89e03f8fd2d4b377cb2c756adb6e1f
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-14 19:41:52 -0600

    utilizar fragment

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,3 +1,10 @@
+import { Fragment } from "react"
+
 export const FirstApp = () => {
-    return <h1>Adrian</h1>
+    return (
+        <Fragment>
+            <h1>Adrian</h1>
+            <p>subtitulo</p>
+        </Fragment>
+    )
 }
