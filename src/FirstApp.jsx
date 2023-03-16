9e916bd65bc38ad01dbd6722fd80a95668599cac
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-15 23:23:28 -0600

    utilizar props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,17 +1,23 @@
-export const FirstApp = () => {
+// export const FirstApp = (props) => {
+export const FirstApp = ({ title, subtitle, valor }) => {
+  // console.log('props', props);
   return (
     <>
       <h1>{Message(name)}</h1>
+      {/* <h2>{props.title}</h2> */}
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <h4>{valor}</h4>
       <code>{JSON.stringify(obj)}</code>
       <p>subtitulo</p>
     </>
   );
 };
 
-const name = 'Adrian';
+const name = "Adrian";
 const obj = {
-    a: 'a',
-    b: 2
-}
+  a: "a",
+  b: 2,
+};
 
 const Message = (name) => `Hola ${name}`;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelloWorldApp></HelloWorldApp>
-    <FirstApp></FirstApp>
+    <FirstApp title="Titulo" subtitle="subtitulo" valor={123}></FirstApp>
   </React.StrictMode>
 );
