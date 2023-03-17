f7108dfe70acfc6fb024117fb576a7726f20909a
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-16 19:19:27 -0600

    agregar intellisense para jest

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,5 +1,11 @@
-test('Esta prueba no debe fallar', () =>{
-    if (1 === 0) {
-        throw new Error('error')
-    }
+describe('Pruebas en DemoComponent', () => {
+    test("Esta prueba no debe fallar", () => {
+        // 1. Inicializacion
+        const message1 = "Hola Mundo";
+        // 2. Estimulo
+        const message2 = message1.trim();
+        // 3. Observar el comportamiento esperado
+        expect(message1).toBe(message2);
+        
+      });
 })
