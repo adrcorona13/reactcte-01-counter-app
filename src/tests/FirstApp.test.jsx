5c16e8fd9b975a31db78eff68e54940352cf8a45
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-22 15:39:44 -0600

    getbytestid y getbytext

--- src/tests/FirstApp.test.jsx
+++ src/tests/FirstApp.test.jsx
@@ -2,20 +2,30 @@ import { render } from "@testing-library/react"
 import { FirstApp } from "../FirstApp"
 
 describe('Pruebas en FirstApp', () => { 
-    test('debe de hacer match con el snapshot', () => { 
-        const title = 'titulo';
-        const { container } = render(<FirstApp title={title}/>);
-        expect( container ).toMatchSnapshot();
-        // console.log(container);
-     })
+    // test('debe de hacer match con el snapshot', () => { 
+    //     const title = 'titulo';
+    //     const { container } = render(<FirstApp title={title}/>);
+    //     expect( container ).toMatchSnapshot();
+    //     // console.log(container);
+    //  })
 
      test('debe de mostrar el titulo en un h1', () => { 
         const title = 'titulo';
-        const { container, getByText } = render(<FirstApp title={title}/>);
-        expect( container ).toMatchSnapshot();
+        const { getByText, getByTestId } = render(<FirstApp title={title}/>);
         expect( getByText(title) ).toBeTruthy();
-        const h1 = container.querySelector('h1');
-        expect(h1.innerHTML).toBe(title);
-        expect(h1.innerHTML).toContain(title);
-     })
+        // const h1 = container.querySelector('h1');
+        // expect(h1.innerHTML).toBe(title);
+        // expect(h1.innerHTML).toContain(title);
+
+        expect(getByTestId('test-title').innerHTML).toBe(title);
+
+     });
+
+     test('debe de mostrar el subtitulo enviado por props', () => { 
+        const title = 'titulo';
+        const subtitle ='subtitulo';
+        const { getAllByText } = render(<FirstApp title={title} subtitle={subtitle}/>);
+
+        expect(getAllByText(subtitle).length).toBe(2);
+      })
  })   
