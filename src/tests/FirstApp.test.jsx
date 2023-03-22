305ecebab154c443a6d00b74fe82bfa7e3d85bf4
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-22 14:34:57 -0600

    probar firstapp

--- src/tests/FirstApp.test.jsx
+++ src/tests/FirstApp.test.jsx
@@ -4,6 +4,18 @@ import { FirstApp } from "../FirstApp"
 describe('Pruebas en FirstApp', () => { 
     test('debe de hacer match con el snapshot', () => { 
         const title = 'titulo';
-        render(<FirstApp title={title}/>);
+        const { container } = render(<FirstApp title={title}/>);
+        expect( container ).toMatchSnapshot();
+        // console.log(container);
+     })
+
+     test('debe de mostrar el titulo en un h1', () => { 
+        const title = 'titulo';
+        const { container, getByText } = render(<FirstApp title={title}/>);
+        expect( container ).toMatchSnapshot();
+        expect( getByText(title) ).toBeTruthy();
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toBe(title);
+        expect(h1.innerHTML).toContain(title);
      })
  })   
